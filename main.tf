5d509e581ba85bdfbaf31dfc7550ffb7885bccc0
PavloPavlichenko <pablopavlichenko@gmail.com>
2022-01-13 18:47:33 +0200

    Added Profile and Policy Role for logging and cloudwatch metric alarm for cpu monitoring

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "web" {
     instance_type          = "t3.micro"
     key_name               = "pasha-key-stockholm"
     vpc_security_group_ids = [aws_security_group.web.id]
+    iam_instance_profile   = aws_iam_instance_profile.logging_profile.name
     user_data              = <<EOF
 #!/bin/bash
 
@@ -23,7 +24,7 @@ echo \
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 
-docker run -d -p 80:80 ipashkayounot/git_sync:latest
+docker run -d --log-driver=awslogs --log-opt awslogs-group=docker-logs -p 80:80 ipashkayounot/git_sync:latest
 docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup -i 20
 
 EOF
@@ -74,4 +75,48 @@ resource "aws_security_group" "web" {
       Name  = "Devops Task SG"
       Owner = "Pasha Pavlichenko"
     }
+}
+
+resource "aws_iam_role" "logging_role" {
+  name = "logging_role"
+  assume_role_policy = jsonencode(
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "logging_policy_role" {
+  name = "logging_policy_role"
+  roles = [aws_iam_role.logging_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+resource "aws_iam_instance_profile" "logging_profile" {
+  name = "logging_profile"
+  role = aws_iam_role.logging_role.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+  alarm_name                = "cpu-utilization"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "120"
+  statistic                 = "Average"
+  threshold                 = "80"
+  alarm_description         = "This metric monitors ec2 cpu utilization"
+  insufficient_data_actions = []
+  dimensions = {
+    InstanceId = aws_instance.web.id
+  }
 }
