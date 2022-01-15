ae814bc8340d62abfa52948fd2a5381bb9d168e4
PavloPavlichenko <pablopavlichenko@gmail.com>
2022-01-15 12:27:14 +0200

    Added bash scirpt

--- main.tf
+++ main.tf
@@ -8,26 +8,7 @@ resource "aws_instance" "web" {
     key_name               = "pasha-key-stockholm"
     vpc_security_group_ids = [aws_security_group.web.id]
     iam_instance_profile   = aws_iam_instance_profile.logging_profile.name
-    user_data              = <<EOF
-#!/bin/bash
-
-apt-get update
-apt-get install \
-    ca-certificates \
-        curl \
-            gnupg \
-                lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io -y
-
-docker run -d --log-driver=awslogs --log-opt awslogs-group=docker-logs -p 80:80 ipashkayounot/git_sync:latest
-docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup -i 20
-
-EOF
+    user_data              = "${file("docker.sh")}"
 
     tags = {
       Name  = "web"
